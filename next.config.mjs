7cbfd63eb2340a39c259063b300cc8d1aa113739
LALA TECH <olaniyanshamsudeenyinka001@gmail.com>
2026-01-11 08:02:41 +0100

    backend api route

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
+        destination: 'https://gadtrack2-0.onrender.com/api/:path*',
       },
     ];
   },
